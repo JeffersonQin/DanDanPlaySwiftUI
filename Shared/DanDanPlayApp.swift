b21a15760b6fc00297c4a06a38b5cfab1bfe68af
JeffersonQin <1247006353@qq.com>
2021-01-08 23:33:37 +0800

    Initial Commit

---
+++ Shared/DanDanPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanDanPlayApp.swift
+//  Shared
+//
+//  Created by JeffersonQin on 2021/1/8.
+//
+
+import SwiftUI
+
+@main
+struct DanDanPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
